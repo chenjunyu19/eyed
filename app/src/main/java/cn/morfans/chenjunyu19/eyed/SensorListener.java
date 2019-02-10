c983ed4efe97a52083bb23938ce8c4cd30cad1db
chenjunyu19 <1335546814@qq.com>
2019-02-10 11:58:23 +0800

    分离 SensorManager 的注册部分

--- app/src/main/java/cn/morfans/chenjunyu19/eyed/SensorListener.java
+++ app/src/main/java/cn/morfans/chenjunyu19/eyed/SensorListener.java
@@ -1,39 +1,61 @@
 package cn.morfans.chenjunyu19.eyed;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
-public class SensorListener implements SensorEventListener {
+class SensorListener {
     private long lastToast;
     private Context context;
+    private SensorManager sensorManager;
+    private SensorEventListener sensorEventListener;
 
     SensorListener(Context context) {
         this.context = context;
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_GRAVITY:
-                if (event.values[2] <= -3 && System.currentTimeMillis() - lastToast >= 2500) {
-                    Toast.makeText(context, "你正在以不健康的姿势使用手机！", Toast.LENGTH_SHORT).show();
-                    lastToast = System.currentTimeMillis();
-                }
-                break;
-            case Sensor.TYPE_LIGHT:
-                if (event.values[0] == 0 && System.currentTimeMillis() - lastToast >= 2500) {
-                    Toast.makeText(context, "你正在黑暗的环境中使用手机！", Toast.LENGTH_SHORT).show();
-                    lastToast = System.currentTimeMillis();
-                }
-                break;
+    private class SensorEventListener implements android.hardware.SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_GRAVITY:
+                    if (event.values[2] <= -3 && System.currentTimeMillis() - lastToast >= 2500) {
+                        Toast.makeText(context, "你正在以不健康的姿势使用手机！", Toast.LENGTH_SHORT).show();
+                        lastToast = System.currentTimeMillis();
+                    }
+                    break;
+                case Sensor.TYPE_LIGHT:
+                    if (event.values[0] == 0 && System.currentTimeMillis() - lastToast >= 2500) {
+                        Toast.makeText(context, "你正在黑暗的环境中使用手机！", Toast.LENGTH_SHORT).show();
+                        lastToast = System.currentTimeMillis();
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
         }
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    void register() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        sensorEventListener = new SensorEventListener();
+        if (sharedPref.getBoolean("enable_gravity", false)) {
+            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (sharedPref.getBoolean("enable_light", false)) {
+            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
 
+    void unregister() {
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
--- app/src/main/java/cn/morfans/chenjunyu19/eyed/eyedService.java
+++ app/src/main/java/cn/morfans/chenjunyu19/eyed/eyedService.java
@@ -1,32 +1,18 @@
 package cn.morfans.chenjunyu19.eyed;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 public class eyedService extends Service {
-    SensorManager sensorManager;
-    SensorEventListener sensorListener;
+    private SensorListener sensorListener;
 
     @Override
     public void onCreate() {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensorListener = new SensorListener(this);
-        if (sharedPref.getBoolean("enable_gravity", false)) {
-            sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
-        }
-        if (sharedPref.getBoolean("enable_light", false)) {
-            sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
-        }
+        sensorListener.register();
     }
 
     @Override
@@ -41,6 +27,6 @@ public class eyedService extends Service {
 
     @Override
     public void onDestroy() {
-        sensorManager.unregisterListener(sensorListener);
+        sensorListener.unregister();
     }
 }
